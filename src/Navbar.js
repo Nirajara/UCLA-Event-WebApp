4050a929a3feb5b08000090ece49269d19536069
Nirajara <nirajara.dun@gmail.com>
2022-11-10 14:26:15 -0800

    Modified background and navigation bar. Background no  longer repeats and is static when scrolling. Navigation bar remains in the top left corner instead of scrolling with everything else.

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,11 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-		<div class = "banner">
-			<button class="button"><Link to="/post" class="text-format">Home</Link></button>
-			<button class="button"><Link to="/signin" class="text-format">Login</Link></button>
-			<button class="button"><Link to="/upload" class="text-format">Post</Link></button>
-		</div>
+	    <div class = "banner">
+	    <button class="button"><Link to="/post" class="text-format">Home</Link></button>
+	    <button class="button"><Link to="/signin" class="text-format">Login</Link></button>
+	    <button class="button"><Link to="/upload" class="text-format">Post</Link></button>
+	    </div>
     );
 }
 
