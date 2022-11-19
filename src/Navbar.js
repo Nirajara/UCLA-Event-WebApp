8a08d3686b24982cf6f16cc2b3a13cbb6c05e7a6
Nirajara <nirajara.dun@gmail.com>
2022-11-18 22:22:59 -0800

    Deleted unnecessary imports and fixed wording and logo in Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css'
 import Container from 'react-bootstrap/Container'
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
 import Button from "react-bootstrap/Button"
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Card from "react-bootstrap/Card"
@@ -20,11 +17,10 @@ const Navbar = () => {
     return (
 	    <Card className="banner">
 			<Card.Img variant="top" src={logo} />
-			<Card.Title>Website Name</Card.Title>
-			<Card.Subtitle>We should probably come up with a name for the website</Card.Subtitle>
+			<Card.Title>UCLA Events</Card.Title>
 			<ButtonGroup vertical>
 				<Button variant="warning" onClick={() => handleClick("post")}>Home</Button>
-				<Button variant="primary" onClick={() => handleClick("signin")}>Login</Button>
+				<Button variant="primary" onClick={() => handleClick("signin")}>Sign In</Button>
 				<Button variant="warning" onClick={() => handleClick("upload")}>Post</Button>
 			</ButtonGroup>
 		</Card>
