8d7d469d7d8c46b3363ef6667aa55ed386af7cf8
Sidharth Alok Girdhar <116860545+sidalok1@users.noreply.github.com>
2022-11-24 21:39:22 -0500

    Added user centric features to navbar
    Need to check if implementation of the "Your Profile" button is correct. Also need to implement logout feature.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './App.css'
-import Container from 'react-bootstrap/Container'
+import {db} from './firebase'
+import { getDoc, doc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Button from "react-bootstrap/Button"
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Card from "react-bootstrap/Card"
@@ -9,22 +12,63 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import logo from './logo.PNG'
 
 const Navbar = () => {
+
+	
+	const auth = getAuth();
+
+    const [user, setUser] = useState({
+        name: "",
+        uid: "",
+    })
+
+	
+
+	useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            getDoc(doc(db, "users", user.uid)).then(user_doc => {
+                const data = user_doc.data();
+                setUser({
+                    uid: user.uid,
+                    name: data.name,
+                });
+            });
+        });
+    });
+
 	const navigate = useNavigate();
  	function handleClick(path) {
     	navigate(path);
  	}
 
+	if(user.uid == "") {
+		console.log("Blank uid")
+		return (
+			<Card className="banner">
+				<Card.Img variant="top" src={logo} />
+				<Card.Title>UCLA Events</Card.Title>
+				<ButtonGroup vertical>
+					<Button variant="warning" onClick={() => handleClick("post")}>Home</Button>
+					<Button variant="primary" onClick={() => handleClick("signin")}>Sign In</Button>
+					<Button variant="warning" onClick={() => handleClick("upload")}>Post</Button>
+				</ButtonGroup>
+			</Card>
+		);
+	} else {
+		console.log(user.uid)
     return (
 	    <Card className="banner">
 			<Card.Img variant="top" src={logo} />
 			<Card.Title>UCLA Events</Card.Title>
 			<ButtonGroup vertical>
 				<Button variant="warning" onClick={() => handleClick("post")}>Home</Button>
-				<Button variant="primary" onClick={() => handleClick("signin")}>Sign In</Button>
+				<Button variant="primary" onClick={() => navigate("/user", { state: { id: user.uid} })}>Your Profile</Button>
 				<Button variant="warning" onClick={() => handleClick("upload")}>Post</Button>
 			</ButtonGroup>
+			<Button variant="outline-primary">Log Out</Button>
 		</Card>
     );
+
+	}
 }
 
 export default Navbar;
